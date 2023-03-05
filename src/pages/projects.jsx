3e84b07cf0f94951f5d4beb8fb73ea7aa27da309
brandonlea <brandonlea810@gmail.com>
2023-03-05 13:58:03 +0000

    Initial commit

---
+++ src/pages/projects.jsx
@@ -0,0 +1,79 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import { Card } from '../components/Card'
+import { SimpleLayout } from '../components/SimpleLayout'
+
+const projects = [
+  {
+    name: 'Force-field',
+    description:
+      'Force-field plugin to push mobs away in all directions.',
+    link: { href: '#', label: 'github.com' },
+  },
+  {
+    name: 'Ultra Core',
+    description:
+      'A Custom Spigot API Ive been working on to make it easier for people.',
+    link: { href: 'https://github.com/brandonlea/UltraCore', label: 'github.com' },
+  },
+  {
+    name: 'Ultra Hub',
+    description:
+      'A all in one Hub plugin for Minecraft.',
+    link: { href: 'https://github.com/brandonlea/UltraHub', label: 'github.com' },
+  },
+  {
+    name: 'Tweet Email Application',
+    description:
+      'Tracks the email status from when you have sent it, this was made for a technical interview.',
+    link: { href: 'https://github.com/brandonlea/TweetEmailApplication', label: 'github.com' },
+  },
+]
+
+function LinkIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+        <title>Projects - Brandon-Lea Price</title>
+        <meta
+          name="description"
+          content="Things I’ve made to show what I can do."
+        />
+      </Head>
+      <SimpleLayout
+        title="Things I’ve made to show what I can do."
+        intro="I've worked on a fair few things over the past years to show what I can do and to show I am willing to try something new."
+      >
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+        >
+          {projects.map((project) => (
+            <Card as="li" key={project.name}>
+              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+                <Card.Link href={project.link.href}>{project.name}</Card.Link>
+              </h2>
+              <Card.Description>{project.description}</Card.Description>
+              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                <LinkIcon className="h-6 w-6 flex-none" />
+                <span className="ml-2">{project.link.label}</span>
+              </p>
+            </Card>
+          ))}
+        </ul>
+      </SimpleLayout>
+    </>
+  )
+}
---
+++ src/pages/uses.jsx
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+
+import { Card } from '../components/Card'
+import { Section } from '../components/Section'
+import { SimpleLayout } from '../components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export default function Uses() {
+  return (
+    <>
+      <Head>
+        <title>Uses - Spencer Sharp</title>
+        <meta
+          name="description"
+          content="Software I use, gadgets I love, and other things I recommend."
+        />
+      </Head>
+      <SimpleLayout
+        title="Software I use, gadgets I love, and other things I recommend."
+        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      >
+        <div className="space-y-20">
+          <ToolsSection title="Workstation">
+            <Tool title="Custom Built Desktop PC">
+              CPU: Ryzen 7 2700x
+              GPU: RTX 3060
+              Ram: 32GB DDR4
+              Storage: 1TB M.2, 3x 500GB SSD
+            </Tool>
+            <Tool title="BenQ 29 Inch monitor">
+              I have 2x monitors so I can work quicker and more efficient.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Development tools">
+            <Tool title="Webstorm">
+              I love using Webstorm when I am doing front end website development like HTML, CSS, JS.
+            </Tool>
+            <Tool title="PHPStorm">
+              I love using PHPStorm when I'm working with PHP projects like Laravel or core PHP it makes it so much easier and efficient.
+            </Tool>
+            <Tool title="IntelliJ">
+              Great software when working with Java it's the ultimate IDE all in one.
+            </Tool>
+          </ToolsSection>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
